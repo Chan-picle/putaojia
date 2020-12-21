6beea7d54e2ab0ff739960139e02cf36fe30acc8
qmhaonan <946794171@qq.com>
2020-12-21 16:11:45 +0800

    list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,9 @@
   <h1>{{ msg }}</h1>
   <button @click="count++">count is: {{ count }}</button>
   <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <ul>
+    <li v-for="item in list" :key="item">{{ item }}</li>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -13,7 +16,8 @@ export default defineComponent({
   },
   data() {
     return {
-      count: 0
+      count: 0,
+      list: ["大佬", "菜菜", "旭旭", "牛牛"]
     }
   }
 })
