9b0bba82c1ead89e2aeec813e8371891b751049c
pan mingxu <1312500862@qq.com>
2020-12-22 20:27:48 +0800

    pan-20201222

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 // import Button from 'vant';
-import { Tabbar, TabbarItem,Button } from 'vant';
+import { Tabbar, TabbarItem, Button, NavBar, Tab, Tabs, Calendar } from 'vant';
 import 'vant/lib/index.css';
 
 
@@ -13,4 +13,8 @@ app.use(router);
 app.use(Button);
 app.use(Tabbar);
 app.use(TabbarItem);
+app.use(NavBar);
+app.use(Tab);
+app.use(Tabs);
+app.use(Calendar);
 app.mount('#app');
