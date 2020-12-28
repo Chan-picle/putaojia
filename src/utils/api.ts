6456eed60170b096cd8bfce7806d0c4359f2045f
Chen JiaYing <chenpickle@163.com>
2020-12-28 23:31:12 +0800

    登陆注册逻辑

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,6 @@ import http from "./http";
 interface paramType{
   [propName:string]:any
 }
-// export const getUser = (params:paramType) => http.get("/user/getuser", params);
 export const getHello = (params:paramType) => http.get("/hello", params);
 
 export const getTimeListApi = (params:paramType) => http.get("/timefirst", params);
@@ -20,3 +19,9 @@ export const getCourseListApi = (params:paramType) => http.get("/product/product
 
 //通过商品id获得商品所有信息params{id:1000}
 export const getProductInfoApi = (params:paramType) => http.post("/product/getproduct", params);
+
+//获得验证码params{phone:"15300000000"}
+export const getVertifyCodeApi = (params:paramType) => http.post("/user/sendcode", params);
+
+//用户登陆/注册params{phone:"15300000000",code:"1236"}
+export const loginApi = (params:paramType) => http.post("/user/login", params);
