75813b0ed9898d30fe9958764e70b13c1150ae8d
Chen JiaYing <chenpickle@163.com>
2020-12-28 12:48:53 +0800

    首页课程渲染

--- src/views/indexTab/Course.vue
+++ src/views/indexTab/Course.vue
@@ -1,26 +1,41 @@
 <template>
-  <product-item />
-  <product-item />
+<div class="product-container" v-if="courseList.length">
+  <product-item v-for="(item,i) in courseList" :key="i" :msg="item" />
+  <!-- <product-item />
+  <product-item /> -->
+</div>
+  
+  <!-- <product-item /> -->
 </template>
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 import ProductItem from "../../components/shouye/ProductItem.vue";
+import {getCourseListApi} from "../../utils/api";
 
 export default defineComponent({
   components: {ProductItem},
   props:{},
   data() {
     return {
-
+      courseList:[]
     };
   },
   computed: {},
-
-  mounted() {},
+  mounted() {
+    getCourseListApi({}).then((res:any)=>{
+      this.courseList = res.result;
+    });
+  },
 
   methods: {}
 });
 </script>
 <style lang="less" scoped>
-
+.product-container{
+  width: 355px;
+  margin:0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
