ac7f003a5891f1049610f5f125894ff5a1829127
pan mingxu <1312500862@qq.com>
2020-12-29 09:11:25 +0800

    pan-添加课的信息

--- src/router/index.ts
+++ src/router/index.ts
@@ -177,17 +177,17 @@ const router = createRouter({
   routes
 });
 router.beforeEach((to, from, next) => {
-  // if (to.path === "/login") {next();}
-  // else { // 判断该路由是否需要登录权限
-  // if (to.meta.requiresAuth && !sessionStorage.getItem("token")) { // 判断当前的token是否存在
-  //   next({
-  //     path: "/login",
-  //     query: {redirect: to.fullPath} // 将跳转的路由path作为参数，登录成功后跳转到该路由
-  //   });
-  // }else{
+  if (to.path === "/login") {next();}
+  else { // 判断该路由是否需要登录权限
+  if (to.meta.requiresAuth && !sessionStorage.getItem("token")) { // 判断当前的token是否存在
+    next({
+      path: "/login",
+      query: {redirect: to.fullPath} // 将跳转的路由path作为参数，登录成功后跳转到该路由
+    });
+  }else{
     next();
-  // }
-  // }
+  }
+  }
 })
 
 export default router
